e6611436b8ea2a9aabe6faf2dc6cea768cf71194
Louise Dennevi <louisedennevi@Louises-MacBook-Pro.local>
2020-02-05 11:04:45 +0100

    mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const commentRouter = require("./router/commentRouter");
-const config = require("./config/config")
-const path = require("path");
-const app = express();
+
+
+
+//listen to port
+const port = process.env.PORT || 8004;
+ 
+mongoose.connect("mongodb+srv://louisedennevi:louise123@todolist-3zvcf.mongodb.net/test?retryWrites=true&w=majority")
+//app is listening to port
+
+app.listen(port);
