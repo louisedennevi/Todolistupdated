f462a34101b019739a3a8b8011cca01a2e351705
Louise Dennevi <louisedennevi@Louises-MBP.mediegruppen.se>
2020-02-06 12:19:49 +0100

    input

--- model/comment.js
+++ model/comment.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 const schemaComment = new mongoose.Schema(
     {
         text: {type:String, required:true, minlength:5},
-        date: { type: Date,  default: Date.now }, 
-        author:{type:String, required:true}
+        // author:{type:String, required:true}
     }
 )
 
